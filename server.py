05e9fb77361954aca3e4dbe25cfb1ee5b888e531
yond5413 <87887705+yond5413@users.noreply.github.com>
2023-03-23 17:18:25 -0400

    new update to server.py
    Just edited some of the strig values for the server

--- server.py
+++ server.py
@@ -35,7 +35,7 @@ DATABASE_PASSWRD = "4577" #TODO ? can you confirm if this right I forgot
 DATABASE_HOST = '34.28.53.86'#"34.148.107.47" # change to 34.28.53.86 if you used database 2 for part 2
 DATABASEURI = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWRD}@{DATABASE_HOST}/project1"
 
-
+#DATABASEURI = "postgresql://rm392:4577@34.28.53.86/project1"
 #
 # This line creates a database engine that knows how to connect to the URI above.
 #
